a1ea4d83630c185cf0ae7c7e4fed8dfb0c37fc12
Katie Nichols <kaylotura@gmail.com>
2017-04-24 21:25:37 -0700

    linter mostly passing

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,29 +2,29 @@
  * Created by kaylo on 4/19/2017.
  */
 import React, { Component, PropTypes } from 'react';
-
+import PropTypes from 'prop-types'
 
 export default class SearchBar extends Component {
-    render() {
-        return (
-        <form>
-            <div>
-                <input
-                    id="searchStringBox"
-                    type="text"
-                    placeholder="Search..."
-                    onChange={ (event) => this.props.handleSearchString(event.target.value)}
+  render () {
+    return (
+      <form>
+        <div>
+          <input
+            id='searchStringBox'
+            type='text'
+            placeholder='Search...'
+            onChange={(event) => this.props.handleSearchString(event.target.value)}
                 />
-            </div>
-            <div>
-                <input
-                    id="filterStockedCheckBox"
-                    type="checkbox"
-                    onClick={this.props.handleFilterStockedToggle}
+        </div>
+        <div>
+          <input
+            id='filterStockedCheckBox'
+            type='checkbox'
+            onClick={this.props.handleFilterStockedToggle}
                 />
                 "Only show products in stock"
             </div>
-        </form>
-        )
-    }
+      </form>
+    )
+  }
 }
